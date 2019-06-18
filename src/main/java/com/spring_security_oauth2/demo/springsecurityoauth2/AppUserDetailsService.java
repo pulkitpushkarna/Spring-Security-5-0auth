1d7f8165df56da208dbf0c4d800c9e260e44a8d7
Pulkit <pulkit@intelligrape.com>
2019-06-18 19:49:34 +0530

    Passing Additional value with auth data

--- src/main/java/com/spring_security_oauth2/demo/springsecurityoauth2/AppUserDetailsService.java
+++ src/main/java/com/spring_security_oauth2/demo/springsecurityoauth2/AppUserDetailsService.java
@@ -6,6 +6,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Service
 public class AppUserDetailsService implements UserDetailsService {
@@ -15,6 +19,8 @@ public class AppUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        System.out.println("acces_token: " + request.getParameter("acces_token"));
         String encryptedPassword = passwordEncoder.encode("pass");
         System.out.println("Trying to authenticate user ::" + username);
         System.out.println("Encrypted Password ::"+encryptedPassword);
